3afd4a5958401f0ac912bc63d75797b0fa67a3c2
Antoine de Chevigne <adechevigne@gmail.com>
2012-08-31 16:04:29 +0200

    shabnag pyparsing

--- pyparser/pyparsing.py
+++ pyparser/pyparsing.py
@@ -22,7 +22,7 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 #from __future__ import generators
-
+#!/usr/bin/env python
 __doc__ = \
 """
 pyparsing module - Classes and methods to define and execute parsing grammars
