4cf9f32d6eafd520ba0960ec17116f9ec2195e05
Antoine de Chevigné <adechevigne@gmail.com>
2012-11-16 21:03:22 +0100

    get aws cred from env

--- application/config/awslib.php
+++ application/config/awslib.php
@@ -50,11 +50,11 @@
 $config['storage']['dev'] = array(
 		// Amazon Web Services Key. Found in the AWS Security Credentials. You can also pass
 		// this value as the first parameter to a service constructor.
-		'key' => 'AKIAJ6S2NOXFEL2GP5FA',
+		'key' => getenv('EC2_ACCESS_KEY'),
 
 		// Amazon Web Services Secret Key. Found in the AWS Security Credentials. You can also
 		// pass this value as the second parameter to a service constructor.
-		'secret' => 'QwH7PrMMsw9HV9o+wBARqEMm248AnJXYTPf9AIqk',
+		'secret' => getenv('EC2_SECRET_KEY'),
 
 		// This option allows you to configure a preferred storage type to use for caching by
 		// default. This can be changed later using the set_cache_config() method.
